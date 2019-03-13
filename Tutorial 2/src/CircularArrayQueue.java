d335890768d44d7af3d940beeb01b752a0ab0b9b
db5n17 <db5n17@soton.ac.uk>
2019-03-13 03:53:15 +0000

    Implemented CircularArrayRing and refactored CircularArrayQueue classes.

--- Tutorial 2/src/CircularArrayQueue.java
+++ Tutorial 2/src/CircularArrayQueue.java
@@ -6,45 +6,47 @@ public class CircularArrayQueue implements MyQueue
 
     private int head = 0;
     private int tail = 0;
+    private int items = 0;
     private int size = DEFAULT_SIZE;
 
-    // Note: An Integer array is used as opposed to an int array since primitives cannot be set to null.
-    Integer[] queue = new Integer[size];
+    private int[] queue = new int[size];
 
     // Function that adds an integer element to the tail of the queue.
     public void enqueue(int in)
     {
-        // If the capacity of the array that is left is less than or equal to zero, the call the doubleQueueSize() supplementary function.
+        // If the capacity of the array that is left is less than or equal to zero, then call the doubleQueueSize() supplementary function.
         if (getCapacityLeft() <= 0)
         {
             doubleQueueSize();
         }
-        // Else, if the tail value is more than or equal to the size, the set the tail back to zero - i.e. loop back to the start of the array.
+        // Else, if the tail variable is more than or equal to the size, then set the tail back to zero - i.e. loop back to the start of the array.
         else if (tail >= size)
         {
             tail = 0;
         }
 
-        // Set the element in the array at the tail index to the value of the 'in' parameter, and increase the tail value by 1.
+        // Set the tail element to the 'in' value, and then increment the tail and items variables.
         queue[tail] = in;
         tail++;
+        items++;
     }
 
     // Function that removes the first integer element (at the head) from the queue, and returns the removed value.
     public int dequeue() throws NoSuchElementException
     {
-        // If the head value is more than or equal to the size, then set the head value back to zero - i.e. loop back to the start of the array.
+        // If the head variable is more than or equal to the size, then set the head variable back to zero - i.e. loop back to the start of the array.
         if (head >= size)
         {
             head = 0;
         }
 
-        // If the value at the head of the queue is not null, set the value to null, increase the head value by 1, and return the value that was removed.
-        if (queue[head] != null)
+        // If the head and tail variables are not the same, or they are the same but the array isn't empty, then remove the head element and return its value.
+        // Increment the head variable and decrement the items variable to correspond to this change.
+        if ((head != tail ) || (head == tail && !isEmpty()))
         {
             int value = queue[head];
-            queue[head] = null;
             head++;
+            items--;
             return value;
         }
         // Otherwise, throw a NoSuchElementException.
@@ -57,40 +59,18 @@ public class CircularArrayQueue implements MyQueue
     // Function that counts the number of populated elements in the queue.
     public int noItems()
     {
-        int items = 0;
-
-        // For each element in the queue, if the value is not null, increment the items value by 1.
-        for (Integer i : queue)
-        {
-            if (i != null)
-            {
-                items++;
-            }
-        }
-
         return items;
     }
 
     // Function that checks to see if the queue is empty.
     public boolean isEmpty()
     {
-        // For each element in the queue, if a populated element is found (i.e. a value is not null), return false.
-        for (Integer i : queue)
-        {
-            if (i != null)
-            {
-                return false;
-            }
-        }
-
-        // Otherwise return true.
-        return true;
+        return (items == 0);
     }
 
     // Function that gets the capacity of the queue that is left before it needs to be resized.
     public int getCapacityLeft()
     {
-        // Calculate the capacity by subtracting the number of items populated from the size of the queue.
         return size - noItems();
     }
 
@@ -98,16 +78,15 @@ public class CircularArrayQueue implements MyQueue
     public void doubleQueueSize()
     {
         // Create a new array called temp, and copy the current queue array to it.
-        Integer[] temp = queue;
+        int[] temp = queue;
 
-        // Double the size variable.
         size = size * 2;
 
         // Recreate the queue array to the new size and copy the contents of temp to it.
-        queue = new Integer[size];
+        queue = new int[size];
         System.arraycopy(temp, 0, queue, 0, temp.length);
 
-        // Reset the head value and set the tail value to the next free location, the length of the old array.
+        // Reset the head variable and set the tail variable to the next free location, the length of the old array.
         head = 0;
         tail = temp.length;
     }
--- Tutorial 2/src/CircularArrayRing.java
+++ Tutorial 2/src/CircularArrayRing.java
@@ -1,58 +1,114 @@
 import java.util.AbstractCollection;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class CircularArrayRing<E> extends AbstractCollection<E> implements Ring<E>
 {
-    private int size;
+    private static final int DEFAULT_SIZE = 10;
+
+    private int capacity;
     private int mark = 0;
-    private ArrayList<E> ring = new ArrayList(size);
+    private ArrayList<E> ring;
 
-    public CircularArrayRing() { }
-
-    public CircularArrayRing(int size)
+    // Default constructor that sets the capacity of the initial ArrayList to the DEFAULT_SIZE constant.
+    public CircularArrayRing()
     {
-        this.size = size;
+        capacity = DEFAULT_SIZE;
+        ring = new ArrayList(capacity);
     }
 
+    // Constructor that allows for an initial user-defined capacity.
+    public CircularArrayRing(int capacity)
+    {
+        this.capacity = capacity;
+        ring = new ArrayList(capacity);
+    }
+
+    // Method that adds an element to the ring, replacing an existing one if the ring is already full.
     public boolean add(E e)
     {
-        if (mark >= size)
+        // If the mark is greater than or equal to the capacity, then set it back to zero.
+        if (mark >= capacity)
         {
             mark = 0;
         }
 
-        if (mark < ring.size())
+        // If the ring is still not at its capacity, then simply add the element to the ring at the next possible location.
+        if (size() < capacity)
         {
-            ring.remove(mark);
+            mark++;
+            return ring.add(e);
         }
 
-        ring.add(mark, e);
+        // Otherwise, replace the element corresponding to the mark index with the passed 'e' argument.
+        ring.set(mark, e);
         mark++;
+
         return true;
     }
 
+    // Function that gets the element at the specified index of the ring.
     public E get(int index) throws IndexOutOfBoundsException
     {
-        int realIndex = size - index - 1;
-
-        if (realIndex >= size || realIndex < 0)
+        // If the provided index is out of range (negative or above the current size of the ring), then throw an IndexOutOfBoundsException.
+        if ((index >= size()) || index < 0)
         {
             throw new IndexOutOfBoundsException();
         }
+        // Else, translate the index into the ArrayList index by subtracting the passed index value and 1 from the mark, and adding 10 should this calculated number be negative.
+        // Then use this newly calculated index value with the ArrayList.get() method to retrieve the correct element.
         else
         {
-            return ring.get(size - index);
+            index = mark - index - 1;
+            return ring.get(index < 0 ? index + size() : index);
         }
     }
 
-    public Iterator<E> iterator()
-    {
-        return ring.iterator();
-    }
-
+    // Function that returns the current size of the ring.
     public int size()
     {
         return ring.size();
+    }
+
+    // Function that creates and returns an iterator for the ring structure.
+    public Iterator<E> iterator()
+    {
+        // Anonymous inner class for the iterator.
+        return new Iterator()
+        {
+            private int index = 0;
+
+            @Override
+            // Method that checks to see if another value exists in the ring based on the current index position.
+            public boolean hasNext()
+            {
+                // If the size minus the index is above 0 then there is at least one more element to iterate to, so return true. Otherwise, return false.
+                return (size() - index) > 0;
+            }
+
+            @Override
+            // Method that returns the next value in the ring.
+            public E next() throws NoSuchElementException
+            {
+                // If there is another value, get the value at the index and then increment the index variable by 1.
+                if (hasNext())
+                {
+                    return get(index++);
+                }
+                // Otherwise, throw a NoSuchElementException.
+                else
+                {
+                    throw new NoSuchElementException();
+                }
+            }
+
+            @Override
+            // This method simply returns an UnsupportedOperationException since values cannot be removed from a ring by its very definition.
+            public void remove() throws UnsupportedOperationException
+            {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
 }
